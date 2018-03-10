218e7730f75b267b0272ce08de1d7059dea7e27c
Travis Allen <travisallen6@gmail.com>
2018-03-09 17:09:39 -0700

    Added some functions to card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -7,27 +7,45 @@ export default class Card extends Component {
         super(props)
         this.state = {
             unMasteredWords: [],
-            masteredWords: []
+            masteredWords: [],
+            currentWord: '',
+            lastWord: '',
         }
         this.btnCorrect = this.btnCorrect.bind(this)
         this.btnIncorrect = this.btnIncorrect.bind(this)
     }
-
+    
+    
     btnCorrect(){
         alert('You are correct!')
     }
-
+    
     btnIncorrect(){
         alert('You are incorrect!')
     }
-
+    
+    cycleWords(){
+        let {unMasteredWords, masteredWords, currentWord,lastWord} = this.state;
+        if(masteredWords.length < 5) {
+            let randIndex = Math.floor(Math.random() * unMasteredWords.length)
+            let removedWord = this.state.unMasteredWords.splice(randIndex,1)
+            this.setState({currentWord: removedWord})
+        }
+        
+    }
+    
+    componentDidMount(){
+        this.setState({unMasteredWords: this.props.wordObject})
+    }
+    
+    
     render(){
         let greenColor='#1cdb8b';
         let redColor='#e54b60';
         return(
             <div className='card-container'>
                 <div className='card-content'>
-                    {this.props.children}
+                    {this.state.currentWord}
                 </div>
             <div className='button-container'>
                 <Button 
